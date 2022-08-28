5aa812a705e4d68428e30bdd9970cb2a1a3d1115
Swapnil Tiwari <swaptiwari@outlook.com>
2022-08-28 21:28:04 +0530

    register onclick added

--- Frontend/src/Components/Register/Register.jsx
+++ Frontend/src/Components/Register/Register.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from 'axios'
 import './Register.css'
 
 const Register = () =>
@@ -19,13 +20,26 @@ const Register = () =>
         })
     }
 
+    const register = () =>
+    {
+        const { firstName, lastName, email, password } = user
+        if (firstName && lastName && email && password)
+        {
+            axios.post("http://localhost:3001/register", user).then(res => alert(res.data))
+
+        } else
+        {
+            alert("Invalid input");
+        }
+    }
+
     return (
         <div className="register">
             <input type="text" name="firstName" value={user.firstName} placeholder="First Name" onChange={handleChange} />
             <input type="text" name="lastName" value={user.lastName} placeholder="Last name" onChange={handleChange} />
             <input type="text" name="email" value={user.email} placeholder="Enter your Email" onChange={handleChange} />
             <input type="password" name="password" value={user.password} placeholder="Enter your Password" onChange={handleChange} />
-            <button >Register</button>
+            <button onClick={register}>Register</button>
             <div>or</div>
             <button>Login</button>
         </div>
