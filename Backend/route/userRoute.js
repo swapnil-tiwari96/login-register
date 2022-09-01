1e37cb0b135b9a49d14d8fb1ed05c303d9831226
Swapnil Tiwari <swaptiwari@outlook.com>
2022-09-01 12:12:12 +0530

    minor change

--- Backend/route/userRoute.js
+++ Backend/route/userRoute.js
@@ -8,6 +8,6 @@ router.post('/login', userController.login);
 
 router.post('/register', userController.register);
 
-router.get('/getData', userController.getData)
+router.get('/getData', checkAuth, userController.getData)
 
 export { router }; 
